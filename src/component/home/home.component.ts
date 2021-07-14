3792338a09cfb2befc0242fb658f596fc0a47005
yangyaqi <yangyaqi@rayvision.com>
2021-07-14 15:07:16 +0800

    fix: d3

--- src/component/home/home.component.ts
+++ src/component/home/home.component.ts
@@ -1,18 +1,35 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, AfterViewInit } from '@angular/core'
+import * as d3 from 'd3'
+
+interface BkgPoint{
+  x: number,
+  y: number,
+}
 
 @Component({
   selector: 'ander-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements AfterViewInit {
 
   constructor() { }
 
-  ngOnInit(): void {
-    console.log()
-    const a:number [] = []
-    a.map(()=>{})
+  ngAfterViewInit(): void {
+    // if (this.bkg === undefined)
+    //   return
+    const bkg = d3.select('#bkg')
+      .append("svg")
+      .attr("width", '100%')
+      .attr("height", '200vh')
+    const bkgLine = d3.line()
+      .curve(d3.curveBasis)
+      // .x(()=>{})
   }
+
+  private bkgLineData: BkgPoint[] = [{x: 0, y: 20}, {x: 150, y: 150}, {x: 300, y: 100}, {x: 450, y: 20}, {x: 600, y: 130}]
+
+
+  // @ViewChild('bkg') private bkg?: ElementRef<Element>
 
 }
